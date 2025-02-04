f61c81f465e9e0a64b43bf5226fd230bdb6915b5
Baron Galileo <barongalileo@gmail.com>
2025-02-04 10:43:01 +0300

    account add

--- backend/messengerzubr/accounts/models.py
+++ backend/messengerzubr/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    name = models.CharField(max_length=255, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+# Сигнал для автоматического создания аккаунта при создании пользователя
+@receiver(post_save, sender=User)
+def create_user_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
+
+# Сигнал для сохранения аккаунта при сохранении пользователя
+@receiver(post_save, sender=User)
+def save_user_account(sender, instance, **kwargs):
+    instance.account.save()
