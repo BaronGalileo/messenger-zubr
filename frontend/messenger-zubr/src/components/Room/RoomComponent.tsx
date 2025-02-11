a019e08f64da5a05047bf00bfd190304180bde49
Baron Galileo <barongalileo@gmail.com>
2025-02-11 19:34:12 +0300

    git tsconfig

--- frontend/messenger-zubr/src/components/Room/RoomComponent.tsx
+++ frontend/messenger-zubr/src/components/Room/RoomComponent.tsx
@@ -1,30 +1,37 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
 import { useEffect } from "react";
 import axios from "axios";
 import { api } from "../../services/api";
+import { removeAuth } from "../../store/authSlice";
 
 export const RoomComponent = () => {
 
     const isAuth = useSelector((state: RootState) => state.auth);
 
     const path = "/messenger/conversations/"
+
+    const dispatch: AppDispatch = useDispatch();
     
 
     useEffect(() => {
         if(isAuth.isAuth) {
             console.log("isAuth")
-            api.get(path).then(res => {console.log(res.data)})
-        
+            api.get(path).then(res => {console.log(res.data)})     
         }
 
     }, [])
+
+    const del = () => {
+            dispatch(removeAuth()) 
+        }
 
     return(
     <>
         {isAuth.isAuth&&
             <div>Комната и компонента</div>}
         <div>Комната без компонента</div>
+        <button onClick={del} >Удалить</button>
     </>
 
     )
