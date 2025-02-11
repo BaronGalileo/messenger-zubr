42eb94d0304cb63b28744b00422d81256a299e7b
Baron Galileo <barongalileo@gmail.com>
2025-02-11 16:35:12 +0300

    test commit

--- frontend/messenger-zubr/src/pages/Room.tsx
+++ frontend/messenger-zubr/src/pages/Room.tsx
@@ -1,6 +1,3 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
 import { RoomComponent } from "../components/Room/RoomComponent";
 
 const RoomPage = () => {
